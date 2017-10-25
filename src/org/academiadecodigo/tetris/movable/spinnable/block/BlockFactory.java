371882825bdc51ce88d6068c1b918d36ed597664
Francisco Machado <franm912@gmail.com>
2017-10-25 22:28:19 +0100

    line shape

--- src/org/academiadecodigo/tetris/movable/spinnable/block/BlockFactory.java
+++ src/org/academiadecodigo/tetris/movable/spinnable/block/BlockFactory.java
@@ -5,6 +5,13 @@ import org.academiadecodigo.tetris.grid.Grid;
 public abstract class BlockFactory {
 
     public static Block getBlock(Grid grid) {
+
+        int rand = (int) (Math.random() * 2);
+
+        if (rand == 0) {
+            return new Line(grid);
+        }
+
         return new Square(grid);
     }
 }
