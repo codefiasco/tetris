4610437fa79b6929a9c23fa64cfdf0e6ccbb8f5a
Francisco Machado <franm912@gmail.com>
2017-10-25 19:52:47 +0100

    created position getters

---
+++ src/org/academiadecodigo/tetris/grid/Grid.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.tetris.grid;
+
+public class Grid {
+}
--- src/org/academiadecodigo/tetris/position/Position.java
+++ src/org/academiadecodigo/tetris/position/Position.java
@@ -1,8 +1,26 @@
 package org.academiadecodigo.tetris.position;
 
+import org.academiadecodigo.tetris.grid.Grid;
+
 public class Position {
+
+    private Grid grid;
 
     private int col;
     private int row;
 
+    public Position(Grid grid, int col, int row) {
+        this.grid = grid;
+
+        this.col = col;
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
 }
