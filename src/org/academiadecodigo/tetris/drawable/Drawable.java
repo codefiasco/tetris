33afa4892a43b258edbed59213f33fe9c728ce86
Francisco Machado <franm912@gmail.com>
2017-10-25 19:34:37 +0100

    position class

--- src/org/academiadecodigo/tetris/drawable/Drawable.java
+++ src/org/academiadecodigo/tetris/drawable/Drawable.java
@@ -1,6 +1,8 @@
 package org.academiadecodigo.tetris.drawable;
 
+import org.academiadecodigo.tetris.position.Position;
+
 public interface Drawable {
     void draw();
-    void getPosition();
+    Position getPosition();
 }
---
+++ src/org/academiadecodigo/tetris/position/Position.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.tetris.position;
+
+public class Position {
+
+    private int col;
+    private int row;
+
+}
