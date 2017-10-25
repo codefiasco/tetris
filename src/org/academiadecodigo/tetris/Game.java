5f4bdabbd9b461db1257d5e5382541232014e797
Francisco Machado <franm912@gmail.com>
2017-10-25 20:15:05 +0100

    keyboard listener

--- src/org/academiadecodigo/tetris/Game.java
+++ src/org/academiadecodigo/tetris/Game.java
@@ -1,14 +1,18 @@
 package org.academiadecodigo.tetris;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.tetris.keyboard_listener.KeyboardListener;
 
 public class Game {
 
     private Rectangle background;
+    private KeyboardListener keyboardListener;
 
     public void init() {
         background = new Rectangle(Constants.PADDING, Constants.PADDING, Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
         background.setColor(Constants.BACKGROUND_COLOR);
         background.fill();
+
+        keyboardListener = new KeyboardListener();
     }
 }
---
+++ src/org/academiadecodigo/tetris/keyboard_listener/KeyboardListener.java
@@ -0,0 +1,46 @@
+package org.academiadecodigo.tetris.keyboard_listener;
+
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+import org.academiadecodigo.tetris.drawable.movable.spinnable.block.Block;
+
+public class KeyboardListener implements KeyboardHandler {
+
+    private Keyboard kb;
+
+    public KeyboardListener(){
+        kb = new Keyboard(this);
+
+        KeyboardEvent key = new KeyboardEvent();
+        key.setKey(KeyboardEvent.KEY_RIGHT);
+        key.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        kb.addEventListener(key);
+
+        key = new KeyboardEvent();
+        key.setKey(KeyboardEvent.KEY_LEFT);
+        key.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        kb.addEventListener(key);
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+
+        switch (keyboardEvent.getKey()) {
+
+            case KeyboardEvent.KEY_RIGHT:
+                System.out.println("R");
+                break;
+
+            case KeyboardEvent.KEY_LEFT:
+                System.out.println("L");
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
+}
