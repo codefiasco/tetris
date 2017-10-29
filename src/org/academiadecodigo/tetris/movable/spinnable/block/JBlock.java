158e0f358832300839ed4846ea9b6026db13d231
Francisco Machado <franm912@gmail.com>
2017-10-29 21:33:13 +0000

    J block spin refactor

--- src/org/academiadecodigo/tetris/movable/spinnable/block/JBlock.java
+++ src/org/academiadecodigo/tetris/movable/spinnable/block/JBlock.java
@@ -39,28 +39,28 @@ public class JBlock extends Block{
 
         int factor = state == Direction.UP ? 1 : -1;
 
-        if (!positions[0].movePermission(positions[0].getCol(), positions[0].getRow() + 2 * factor) ||
-                !positions[1].movePermission(positions[1].getCol() + factor, positions[1].getRow() + factor) ||
-                !positions[2].movePermission(positions[2].getCol() + 2 * factor, positions[2].getRow()) ||
-                !positions[3].movePermission(positions[3].getCol() + 3 * factor, positions[3].getRow() + factor)) {
+        if (!positions[0].movePermission(positions[0].getCol(), positions[0].getRow() + factor) ||
+                !positions[1].movePermission(positions[1].getCol() + factor, positions[1].getRow()) ||
+                !positions[2].movePermission(positions[2].getCol() + 2 * factor, positions[2].getRow() - factor) ||
+                !positions[3].movePermission(positions[3].getCol() + 3 * factor, positions[3].getRow())) {
             return;
         }
 
         positions[0].clear();
-        positions[0].moveTo(positions[0].getCol(), positions[0].getRow() + 2 * factor);
-        representations[0].translate(0, Constants.CELL_SIZE * 2 * factor);
+        positions[0].moveTo(positions[0].getCol(), positions[0].getRow() + factor);
+        representations[0].translate(0, Constants.CELL_SIZE * factor);
 
         positions[1].clear();
-        positions[1].moveTo(positions[1].getCol() + factor, positions[1].getRow() + factor);
-        representations[1].translate(Constants.CELL_SIZE * factor, Constants.CELL_SIZE * factor);
+        positions[1].moveTo(positions[1].getCol() + factor, positions[1].getRow());
+        representations[1].translate(Constants.CELL_SIZE * factor, 0);
 
         positions[2].clear();
-        positions[2].moveTo(positions[2].getCol() + 2 * factor, positions[2].getRow());
-        representations[2].translate(Constants.CELL_SIZE * 2 * factor, 0);
+        positions[2].moveTo(positions[2].getCol() + 2 * factor, positions[2].getRow() - factor);
+        representations[2].translate(Constants.CELL_SIZE * 2 * factor, Constants.CELL_SIZE * -factor);
 
         positions[3].clear();
-        positions[3].moveTo(positions[3].getCol() + 3 * factor, positions[3].getRow() + factor);
-        representations[3].translate(Constants.CELL_SIZE * 3 * factor, Constants.CELL_SIZE * factor);
+        positions[3].moveTo(positions[3].getCol() + 3 * factor, positions[3].getRow());
+        representations[3].translate(Constants.CELL_SIZE * 3 * factor, 0);
 
         state = state == Direction.UP ? Direction.RIGHT : Direction.LEFT;
     }
